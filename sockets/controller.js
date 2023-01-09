5855761b47fa2388043f179771ad1b14dd78087c
Albert Mora <albertm@pickerexpress.com>
2023-01-09 00:00:00 -0500

    Initializing project

---
+++ sockets/controller.js
@@ -0,0 +1,26 @@
+
+
+
+const socketController = (socket) => {
+    
+    console.log('Client connected', socket.id );
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected', socket.id );
+    });
+
+    socket.on('send-message', ( payload, callback ) => {
+        
+        const id = 8128128;
+        callback( id );
+
+        socket.broadcast.emit('send-message', payload );
+
+    })
+
+}
+
+module.exports = {
+    socketController
+}
+
